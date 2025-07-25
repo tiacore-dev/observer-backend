08e4e3753ae1809c2b4d9539b3ac5a9ac527b681
Viktoria <v.s.polikanova@gmail.com>
2025-07-25 13:46:12 +0700

    деплоим latest

--- app/__init__.py
+++ app/__init__.py
@@ -43,11 +43,7 @@ def create_app(config_name: ConfigName) -> FastAPI:
                 queue_name="observer-service",
                 routing_keys=["user.*"],
             )
-            task = asyncio.create_task(
-                consumer.connect_and_consume(
-                    partial(handle_user_event, settings=settings)
-                )
-            )
+            task = asyncio.create_task(consumer.connect_and_consume(partial(handle_user_event, settings=settings)))
             app.state.rabbit_task = task
 
         yield
